c7f5538ad1519c46f3aadfe6af2773efdd46c769
AntonPiekhotin <pehotin.anton@gmail.com>
2023-12-08 17:31:48 +0200

    initial commit

---
+++ src/main/java/com/parking/JustPark/JustParkApplication.java
@@ -0,0 +1,13 @@
+package com.parking.JustPark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustParkApplication.class, args);
+	}
+
+}
