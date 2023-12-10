671389a8bb58a8048a1ba2eb7cf6c61023132030
AntonPiekhotin <pehotin.anton@gmail.com>
2023-12-10 17:16:07 +0200

    auth doesnt works

---
+++ src/main/java/com/parking/JustPark/config/JwtGenerator.java
@@ -0,0 +1,10 @@
+package com.parking.JustPark.config;
+
+import org.springframework.security.core.Authentication;
+
+public class JwtGenerator {
+
+    public String generateToken(Authentication authentication){
+        return null;
+    }
+}
--- src/main/java/com/parking/JustPark/config/SecurityConfig.java
+++ src/main/java/com/parking/JustPark/config/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.parking.JustPark.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,13 +19,32 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
-                            auth.requestMatchers("users/secured").authenticated();
+                            auth.requestMatchers("auth/**").permitAll();
                             auth.requestMatchers("users/hello/**").permitAll();
+                            auth.requestMatchers("users/secured/**").authenticated();
                         }
                 )
-                .oauth2Login(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults())
+//                .oauth2Login(Customizer.withDefaults())
+//                .formLogin(Customizer.withDefaults())
                 .build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtGenerator jwtGenerator() {
+        return new JwtGenerator();
+    }
+
 }
