07863ee94e875ad1c65a236ff92f02e90e979ab2
AntonPiekhotin <pehotin.anton@gmail.com>
2023-12-22 02:45:32 +0200

    rating repo

---
+++ src/main/java/com/parking/JustPark/repository/ParkingRatingRepository.java
@@ -0,0 +1,7 @@
+package com.parking.JustPark.repository;
+
+import com.parking.JustPark.entity.ParkingRating;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParkingRatingRepository extends JpaRepository<ParkingRating, Long> {
+}
